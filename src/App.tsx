eeb39b2c3961b1df3cadd37ec0fe877364e659d2
VanoKi <kirsvano@gmail.com>
2025-04-08 18:50:48 +0300

    feat: add Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Header.tsx";
 
 
 // Hi Guys!
@@ -126,7 +127,7 @@ function App() {
 
     return (
         <div className="App">
-
+            <Header/>
         </div>
     );
 }
