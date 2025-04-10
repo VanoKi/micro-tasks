e2fa6686ae6746a9b27546dab7cf1118aad1207b
VanoKi <kirsvano@gmail.com>
2025-04-10 14:31:22 +0300

    feat: change whitespace to dash

--- src/App.tsx
+++ src/App.tsx
@@ -127,23 +127,26 @@ function App() {
     //     ]
     // }
     const [money, setMoney] = useState([
-        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
-        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
-        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
     ])
+
+    let currentMoney = money.filter(filteredMoney => filteredMoney.banknots = 'RUBLS')
+
     return (
         <>
             <ul>
-                {money.map((objFromMoneyArr) => {
+                {money.map((objFromMoneyArr, index) => {
                     return (
-                        <li>
-                            <span>{objFromMoneyArr.banknots}</span>
-                            <span>{objFromMoneyArr.value}</span>
+                        <li key={index}>
+                            <span>{objFromMoneyArr.banknots}--</span>
+                            <span>{objFromMoneyArr.value}--</span>
                             <span>{objFromMoneyArr.number}</span>
                         </li>
                     )
@@ -154,31 +157,6 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //--------------------------------------------------------------
