42e9b4f00565ea85594db155f4f8edde442834f2
VanoKi <kirsvano@gmail.com>
2025-04-10 14:23:28 +0300

    feat: add money

--- src/App.tsx
+++ src/App.tsx
@@ -126,21 +126,29 @@ function App() {
     //         'Micheal Talbot95',
     //     ]
     // }
-    // let a = 1
-    let [a, setA] = useState(1)
-    const onclickHandler = () => {
-        setA(++a)
-        // ++a
-        console.log(a)
-    }
-    const zero = () => {
-        setA( a = 0)
-    }
+    const [money, setMoney] = useState([
+        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+    ])
     return (
         <>
-            <h1>{a}</h1>
-            <button onClick={onclickHandler}>number</button>
-            <button onClick={zero}>0</button>
+            <ul>
+                {money.map((objFromMoneyArr) => {
+                    return (
+                        <li>
+                            <span>{objFromMoneyArr.banknots}</span>
+                            <span>{objFromMoneyArr.value}</span>
+                            <span>{objFromMoneyArr.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
         </>
     );
 }
