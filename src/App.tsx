81340fdbc05e5f2e865935f5991d2ce480bb953a
VanoKi <kirsvano@gmail.com>
2025-04-10 16:29:59 +0300

    feat: create NewNewComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import React, {useState} from 'react';
-import './App.css';
-import {Header} from "./site/Header.tsx";
-import {Body} from "./site/Body.tsx";
-import {Footer} from "./site/Footer.tsx";
-import {NewComponent} from "./site/NewComponent.tsx";
-import {MouseEvent} from "react";
-import {Button} from "./site/components/Button.tsx";
+import {NewNewComponent} from "./site/NewNewComponent.tsx";
 
 // Hi Guys!
 // Let's reinforce our current session!
 // -You have 2 arrays. You should create a new component TASKS, where you will render these arrays.
 // -Don't forget to assign types to our data.
-
-type FilterType = 'All' | 'Dollars' | 'RUBLS'
 
 function App() {
     // const data1 = {
@@ -128,48 +119,7 @@ function App() {
     //         'Micheal Talbot95',
     //     ]
     // }
-    const [money, setMoney] = useState([
-        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-    ])
-    const [filter, setFilter] = useState<FilterType>('All')
-    let currentMoney = money
-    if (filter === 'RUBLS') {
-        currentMoney = money.filter(filteredMoney => filteredMoney.banknots === 'RUBLS')
-    }
-    if (filter === 'Dollars') {
-        currentMoney = money.filter(filteredMoney => filteredMoney.banknots === 'Dollars')
-    }
-    const onClickFilterHandler = (nameBtn: FilterType) => {
-        console.log(nameBtn)
-        setFilter(nameBtn)
-    }
-    return (
-        <>
-            <ul>
-                {currentMoney.map((objFromMoneyArr, index) => {
-                    return (
-                        <li key={index}>
-                            <span>{objFromMoneyArr.banknots}--</span>
-                            <span>{objFromMoneyArr.value}--</span>
-                            <span>{objFromMoneyArr.number}</span>
-                        </li>
-                    )
-                })}
-            </ul>
-            <div style={{display: 'flex', flexDirection: 'row', gap: '20px'}}>
-                <button onClick={() => onClickFilterHandler('All')}>All</button>
-                <button onClick={() => onClickFilterHandler('RUBLS')}>Roubles</button>
-                <button onClick={() => onClickFilterHandler('Dollars')}>Dollars</button>
-            </div>
-        </>
-    );
+    <NewNewComponent />
 }
 
 export default App;
---
+++ src/site/NewNewComponent.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from "react";
+
+type FilterType = 'All' | 'Dollars' | 'RUBLS'
+
+export const NewNewComponent = () => {
+    const money = [
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ]
+    const [filter, setFilter] = useState<FilterType>('All')
+    let currentMoney = money
+    if (filter === 'RUBLS') {
+        currentMoney = money.filter(filteredMoney => filteredMoney.banknots === 'RUBLS')
+    }
+    if (filter === 'Dollars') {
+        currentMoney = money.filter(filteredMoney => filteredMoney.banknots === 'Dollars')
+    }
+    const onClickFilterHandler = (nameBtn: FilterType) => {
+        console.log(nameBtn)
+        setFilter(nameBtn)
+    }
+    return (
+        <>
+            <ul>
+                {currentMoney.map((objFromMoneyArr, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{objFromMoneyArr.banknots}--</span>
+                            <span>{objFromMoneyArr.value}--</span>
+                            <span>{objFromMoneyArr.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div style={{display: 'flex', flexDirection: 'row', gap: '20px'}}>
+                <button onClick={() => onClickFilterHandler('All')}>All</button>
+                <button onClick={() => onClickFilterHandler('RUBLS')}>Roubles</button>
+                <button onClick={() => onClickFilterHandler('Dollars')}>Dollars</button>
+            </div>
+        </>
+    );
+}
