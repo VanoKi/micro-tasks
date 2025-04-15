227178a5b20a27c7560b6596ed2b0b7a7a0166ed
VanoKi <kirsvano@gmail.com>
2025-04-15 18:40:06 +0300

    feat: add changeEvent

--- src/site/components/FullInput.tsx
+++ src/site/components/FullInput.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 export const FullInput = () => {
+    let [title, setTitle] = useState('')
+    const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+
+    }
     return (
         <div>
-            <input type="text"/>
-            <button>+</button>
+            <input type="text" onChange={onChangeInputHandler}/>
+            <button onClick={() => {}}>+</button>
         </div>
     );
 };
