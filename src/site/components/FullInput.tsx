5534e77f0b2c0e26885d436bf1c6672b02d69250
VanoKi <kirsvano@gmail.com>
2025-04-15 18:45:55 +0300

    feat: add setTitle

--- src/site/components/FullInput.tsx
+++ src/site/components/FullInput.tsx
@@ -2,8 +2,9 @@ import React, {ChangeEvent, useState} from 'react';
 
 export const FullInput = () => {
     let [title, setTitle] = useState('')
+    console.log(title)
     const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
-
+        setTitle(e.currentTarget.value)
     }
     return (
         <div>
